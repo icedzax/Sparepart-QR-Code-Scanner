760fe395dc5e7a3d7747d52412bafe98be3bcf44
sirikarn sangjae <siirikarn.s@zubbsteel.com>
2022-06-23 08:27:33 +0700

    SQ-4 Vertical css

---
+++ src/views/Mainpage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Vertical />
+  </div>
+</template>
+<script>
+import Vertical from "../components/Vertical.vue";
+export default {
+  components: { Vertical },
+};
+</script>
+<style></style>
+/
