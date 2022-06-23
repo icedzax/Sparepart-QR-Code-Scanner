0cee57fd93781a0650d0e9d11c9475d8df3f509e
sirikarn sangjae <siirikarn.s@zubbsteel.com>
2022-06-23 08:52:25 +0700

    SQ-3 Horizon css

--- src/views/Mainpage.vue
+++ src/views/Mainpage.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <Vertical />
+  <div id="demo">
+    <div v-if="SizeNow < 600">
+      <Vertical />
+    </div>
+    <div v-else>
+      <Horizon />
+    </div>
   </div>
 </template>
 <script>
 import Vertical from "../components/Vertical.vue";
+import Horizon from "../components/Horizontal.vue";
 export default {
-  components: { Vertical },
+  components: { Vertical, Horizon },
+  computed: {
+    SizeNow() {
+      return screen.width;
+    },
+  },
 };
 </script>
 <style></style>
