44c12fb20c3fcbf6a458e56c5c8616ce266e57a1
ize <icedza@gmail.com>
2022-11-29 16:05:59 +0700

    minor fixed

--- src/components/Matdata.vue
+++ src/components/Matdata.vue
@@ -23,16 +23,11 @@
       v-for="(item, index) in state.data"
       :key="index"
       class="flex justify-around px-1"
+      v-show="newplant.includes(item.WERKS)"
     >
-      <span class="w-full text-left" v-show="newplant.includes(item.WERKS)">{{
-        item.CHARG
-      }}</span>
-      <span class="w-full text-center" v-show="newplant.includes(item.WERKS)">{{
-        item.WERKS
-      }}</span>
-      <span class="w-full text-right" v-show="newplant.includes(item.WERKS)">{{
-        item.STOCK
-      }}</span>
+      <span class="w-full text-left">{{ item.CHARG }}</span>
+      <span class="w-full text-center">{{ item.WERKS }}</span>
+      <span class="w-full text-right">{{ item.STOCK }}</span>
     </div>
 
     <div class="flex justify-around px-1 font-bold">
@@ -44,60 +39,64 @@
 </template>
 
 <script setup>
-import { computed, ref } from "vue";
-import { state } from "../state";
-const selectPlant = ref();
-const MAKTX = computed(() => {
-  return state.data.length > 0 ? state.data[0].MAKTX : "";
-});
-
-// const sum = state.data.reduce((acc, curr) => {
-//   return parseInt(acc) + parseInt(curr.STOCK);
-// }, 0);
-
-const sum = ref(0);
-
-const newplant = ref([]);
-const Allplant = Array();
-function getPlantButton() {
-  state.data.filter((x) => {
-    if (!Allplant.includes(x.WERKS)) {
-      Allplant.push(x.WERKS);
-    }
-
-    if (!newplant.value.includes(x.WERKS)) {
-      newplant.value.push(x.WERKS);
-    }
-
-    sum.value += parseInt(x.STOCK);
+  import { computed, ref } from "vue";
+  import { state } from "../state";
+  const selectPlant = ref();
+  const MAKTX = computed(() => {
+    return state.data.length > 0 ? state.data[0].MAKTX : "";
   });
-}
-getPlantButton();
 
-const hideData = (i) => {
-  var element = document.getElementById(i);
-  if (newplant.value.includes(i)) {
-    const searchPlant = newplant.value.findIndex((item) => {
-      return item == i;
-    });
-    newplant.value.splice(searchPlant, 1);
-    element.classList.remove("bg-emerald-400");
-    element.classList.add("bg-gray-200");
-  } else {
-    newplant.value.push(i);
-    element.classList.add("bg-emerald-400");
-    element.classList.remove("bg-gray-200");
-  }
-  sum.value = 0;
-  state.data.map((data) => {
-    newplant.value.map((ind) => {
-      if (ind == data.WERKS) {
-        console.log("stock::", ind, "-", data.STOCK);
-        sum.value += parseInt(data.STOCK);
+  // const sum = state.data.reduce((acc, curr) => {
+  //   return parseInt(acc) + parseInt(curr.STOCK);
+  // }, 0);
+
+  const sum = ref(0);
+
+  const newplant = ref([]);
+  const Allplant = Array();
+  function getPlantButton() {
+    state.data.filter((x) => {
+      if (!Allplant.includes(x.WERKS)) {
+        Allplant.push(x.WERKS);
       }
+
+      if (!newplant.value.includes(x.WERKS)) {
+        newplant.value.push(x.WERKS);
+      }
+
+      sum.value += parseInt(x.STOCK);
     });
-  });
-};
+  }
+  getPlantButton();
+
+  const hideData = (i) => {
+    var element = document.getElementById(i);
+    if (newplant.value.includes(i)) {
+      const searchPlant = newplant.value.findIndex((item) => {
+        return item == i;
+      });
+      newplant.value.splice(searchPlant, 1);
+      element.classList.remove("bg-emerald-400");
+      element.classList.remove("text-black");
+      element.classList.add("bg-gray-200");
+      element.classList.add("text-gray-400");
+    } else {
+      newplant.value.push(i);
+      element.classList.add("bg-emerald-400");
+      element.classList.add("text-black");
+      element.classList.remove("bg-gray-200");
+      element.classList.remove("text-gray-400");
+    }
+    sum.value = 0;
+    state.data.map((data) => {
+      newplant.value.map((ind) => {
+        if (ind == data.WERKS) {
+          // console.log("stock::", ind, "-", data.STOCK);
+          sum.value += parseInt(data.STOCK);
+        }
+      });
+    });
+  };
 </script>
 
 <style></style>
--- src/hooks/useMat.js
+++ src/hooks/useMat.js
@@ -8,7 +8,7 @@ const useFindMat = (init) => {
     try {
       const response = await axios.post(
         "https://hook.zubbsteel.com/line-ci/qt/v1/store",
-        { matnr: "VRCI00225" }
+        { matnr: qr }
       );
       getMat.data = response.data;
       state.data = getMat.data;
